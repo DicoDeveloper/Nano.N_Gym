5f1f812f8fe326bd6ff22692dd5657b601a96db7
Leo_Developer <dico_4last@outlook.com>
2018-03-02 14:33:57 -0300

    Editado Pagamento

--- N_Base.Entity/Objects/Pagamento.cs
+++ N_Base.Entity/Objects/Pagamento.cs
@@ -5,22 +5,19 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace N_Base.Entity.Objects
 {
-    [Table("PAGAMENTOS")]
     public class Pagamento
     {
         #region Popriedades
         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        [Column("ID_PAGAMENTO")]
         public long Id { get; set; }
-        [Column("TIPO_OPERACAO"), Required]
+        [Required]
         public TipoOperacaoPagamento TipoOperacao { get; set; }
-        [Column("FORMA_PAGAMENTO"), Required]
+        [Required]
         public FormaPagamento FormaPago { get; set; }
-        [Range(0.01, 999999.99, ErrorMessage = "Valor de pagamento não pode ser zero"), Column("VAL_PAGAMENTO"), Required]
+        [Range(0.01, 999999.99, ErrorMessage = "Valor de pagamento não pode ser zero"), Required]
         public decimal Valor { get; set; }
-        [Range(0, 999999.99), Required, Column("VAL_TROCO")]
+        [Range(0, 999999.99), Required]
         public decimal Troco { get; set; }
-        [Column("DAT_PAGAMENTO")]
         public DateTime DataPago { get; set; }
         #endregion
     }
