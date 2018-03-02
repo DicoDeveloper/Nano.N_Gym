2bb54bb70980bfa2cbacc89c5a511df06827a052
Leo_Developer <dico_4last@outlook.com>
2018-03-02 14:33:47 -0300

    Editado Funcionario

--- N_Base.Entity/Objects/Funcionario.cs
+++ N_Base.Entity/Objects/Funcionario.cs
@@ -3,17 +3,13 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace N_Base.Entity.Objects
 {
-    [Table("FUNCIONARIOS")]
     public class Funcionario : Pessoa
     {
         #region Propriedades
-        [Column("ID_CARGO"), ForeignKey("Cargo")]
+        [ForeignKey("Cargo")]
         public long IdCargo { get; set; }
-        [Column("DAT_ADMISSAO")]
         public DateTime DataAdmissao { get; set; }
-        [Column("DAT_DESLIGAMENTO")]
-        public DateTime DataDesligamento { get; set; }        
-        [Column("FOTO_FUNCIONARIO")]
+        public DateTime DataDesligamento { get; set; }
         public string Foto { get; set; }
         public virtual Cargo Cargo { get; set; }
         #endregion
