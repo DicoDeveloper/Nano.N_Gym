d108befa4f46890838f25ef0ac2964f2d61acb52
Leo_Developer <dico_4last@outlook.com>
2018-03-02 14:32:46 -0300

    Editado Cliente

--- N_Base.Entity/Objects/Cliente.cs
+++ N_Base.Entity/Objects/Cliente.cs
@@ -2,11 +2,9 @@
 
 namespace N_Base.Entity.Objects
 {
-    [Table("CLIENTES")]
     public class Cliente : Pessoa
     {
         #region Propriedades
-        [Column("FOTO_CLIENTE")]
         public string Foto { get; set; }
         #endregion
     }
