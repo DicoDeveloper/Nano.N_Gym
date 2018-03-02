d4f83941658311ee69266d9430a1a5d065fff98a
Leo_Developer <dico_4last@outlook.com>
2018-03-02 14:31:49 -0300

    Editado Auditoria

--- N_Base.Entity/Objects/Auditoria.cs
+++ N_Base.Entity/Objects/Auditoria.cs
@@ -7,29 +7,19 @@ using N_Base.Entity.Enums.Data;
 
 namespace N_Base.Entity.Objects
 {
-    [Table("AUDITORIAS")]
     public class Auditoria
     {
         #region Propriedades
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        [Column("ID_AUDITORIA")]
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public long Id { get; set; }
-        [Column("TIPO_OPERACAO")]
         public TipoOperacao TipoOperacao { get; set; }
-        [Column("OQUE")]
         public string Oque { get => Oque; set => Oque = value.Length > 60 ? value.Substring(0, 60) : value; }
-        [Column("QUANDO")]
         public DateTime Quando { get; set; }
         [ForeignKey("Usuario")]
-        [Column("ID_USUARIO")]
         public long IdUsuario { get; set; }
         [MaxLength(60)]
-        [Column("ONDE")]
         public string Onde { get => Onde; set => Onde = value.Length > 60 ? value.Substring(0, 60) : value; }
-        [Column("TABELA")]
         public TabelaBaseDados Tabela { get; set; }
-        [Column("OBJETOS_AUDITORIA_JSON")]
         public string ObjetosJson { get; set; }
         public virtual Usuario Usuario { get; set; }
         #endregion
