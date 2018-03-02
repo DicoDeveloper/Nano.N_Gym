848328ea40f9c072e44aae9864687195dc691c27
Leo_Developer <dico_4last@outlook.com>
2018-03-02 14:34:33 -0300

    Editado SystemConfig

--- N_Base.Entity/Objects/SystemConfig.cs
+++ N_Base.Entity/Objects/SystemConfig.cs
@@ -3,17 +3,14 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace N_Base.Entity.Objects
 {
-    [Table("SYSTEM_CONFIGS")]
     public class SystemConfig
     {
         #region Propriedades
-        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity), Column("ID_SYSTEM_CONFIG")]
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public long Id { get; set; }
-        [MaxLength(100), Column("DESCRICAO")]
+        [MaxLength(100)]
         public string Descricao { get => Descricao; set => Descricao = value.Length > 50 ? value.Substring(0, 50) : value; }
-        [Column("VAL_CAMPO")]
         public string ValorCampo { get; set; }
-        [Column("OBSERVACAO")]
         public string Observacao { get; set; }
         #endregion
     }
