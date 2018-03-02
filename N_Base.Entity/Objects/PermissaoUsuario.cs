be55e0a1644c649753d9f9f97ef12c1152e12952
Leo_Developer <dico_4last@outlook.com>
2018-03-02 14:34:12 -0300

    Editado PermissaoUsuario

--- N_Base.Entity/Objects/PermissaoUsuario.cs
+++ N_Base.Entity/Objects/PermissaoUsuario.cs
@@ -3,11 +3,10 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace N_Base.Entity.Objects
 {
-    [Table("PERMISSOES_USUARIOS")]
     public class PermissaoUsuario
     {
         #region Propriedades
-        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity), Column("ID_PERMISSAO")]
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public long Id { get; set; }
         #endregion
     }
